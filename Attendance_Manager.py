3a3147edd3728ddc083ab5dab9329f7451155e19
Arjun Bajaj <arjunbajaj2000@gmail.com>
2020-10-27 20:21:00 +0530

    Create First Page

--- Attendance_Manager.py
+++ Attendance_Manager.py
@@ -1,14 +1,31 @@
 from tkinter import * #For designing of front end
-from HelperFunctions import *   # utility functions
+from PIL import ImageTk, Image #For loading images
 #Add more libraries here
 
 class AttendanceManager(object):
 
     def __init__(self):
         self.root = Tk()   #root widget for the app window
-        self.root.geometry("1200X800")
+        self.root.geometry("1200x800")
         self.root.title("Attendance Manager")
         self.root.resizable(0,0)  #to disable resizing of root window to avoid distortion
+        self.BackgroundImage(self.root,"First")
+        self.creat_MainFrame("First")
+        self.root.mainloop()
+
+    def creat_MainFrame(self,page="Any"):
+        self.main_frame = Frame(self.root, height=800, width=1200)
+        self.main_frame.place(x=0, y=0)
+        self.BackgroundImage(self.main_frame,page)
+
+    def BackgroundImage(self,frame,page="Any"):
+        if page == "First":
+            self.bg_image = ImageTk.PhotoImage(file="Images/Background_Front.jpg")
+        else:
+            self.bg_image = ImageTk.PhotoImage(file="Images/Background.jpg")
+        self.bg_label = Label(frame, image=self.bg_image)
+        self.bg_label.place(x=0, y=0)
+
 
 if __name__ == '__main__':
     AttendanceManager()
