1106dd9a04418dfee3d29f2b4e7213a56ac03620
Anirudh <anirudhsinghh26@gmail.com>
2020-10-30 14:10:35 +0530

    1.update Semester optionmenu  2.update BCA 6 Sems and B.Tech 8 Sems

--- Face_Recognition.py
+++ Face_Recognition.py
@@ -1,12 +1,12 @@
-from tkinter import * #For designing of front end
-from PIL import ImageTk, Image #For loading images
+from tkinter import *  #For designing of front end
+from PIL import ImageTk, Image  #For loading images
 from tkinter import font as tkFont
-#Add more libraries here
+  #Add more libraries here
 
 class AttendanceManager(object):
 
     def __init__(self):
-        self.root = Tk()   #root widget for the app window
+        self.root = Tk()  #root widget for the app window
         self.root.geometry("1200x800")
         self.root.title("Attendance Manager")
         self.root.resizable(0,0)  #to disable resizing of root window to avoid distortion
@@ -16,6 +16,10 @@ class AttendanceManager(object):
         self.TextFont = tkFont.Font(family="Courier New", size=15, weight="bold")
         self.FirstPage()
         self.root.mainloop()
+
+#################################################################################################################
+############################################### 1st Page ########################################################
+#################################################################################################################
 
     def FirstPage(self):
         self.content_frame = Frame(self.main_frame,height=300,width=700,bg="Black")
@@ -31,6 +35,28 @@ class AttendanceManager(object):
                                   fg="Black",
                                   command= exit, font=self.ButtonFont, justify=RIGHT, height=1, width=8)
         self.ExitButton.place(x=600, y=240)
+
+###################################################################################################################
+############################################## 2nd Page ###########################################################
+###################################################################################################################
+
+    def AboutPage(self):
+        self.content_frame.destroy()  # Just destroy the content frame as the heading is needed
+        self.content_frame = Frame(self.main_frame, height=300, width=700, bg="Black")
+        self.content_frame.place(x=500, y=450)  # Place below heading
+
+
+        self.about = "Attendance Manager \n\n This is an application that can mark as well as manage your attendance. It confirms your identity by opening your webcam and using our face recognition model to confirm your identity and automatically mark your attendance.Also you can check your previous attendance. \n This application is developed by Arjun Bajaj and Anirudh"
+        self.AboutInfo = Message(self.content_frame, bg="Black", fg="White", font=self.TextFont, justify=CENTER,width=600, text=self.about)
+        self.AboutInfo.place(x=0, y=0)
+
+
+        self.BackButton = Button(self.content_frame, text="Back", activebackground="grey", bd=3, bg="White",fg="Black",command=self.FirstPage, font=self.ButtonFont, justify=CENTER, height=1, width=5)
+        self.BackButton.place(x=400, y=250)
+
+######################################################################################################################
+################################################ 3rd Page ############################################################
+######################################################################################################################
 
     def ProceedPage(self):
         self.main_frame.destroy()  #Destroy the entire main frame and create a new page
@@ -60,61 +86,108 @@ class AttendanceManager(object):
         self.SemesterLabel = Label(self.main_frame, text="Semester   :", bg="black", fg="white",font=self.TextFont)
         self.SemesterLabel.place(x=600, y=360)
 
-        self.EnterSemester = Entry(self.main_frame,bd=3,bg="black",fg="white")
+        self.SelectSemester = StringVar(self.main_frame)
+        self.SelectSemester.set("Select")
+        self.EnterSemester = OptionMenu(self.main_frame, self.SelectSemester, "I", "II", "III", "IV", "V", "VI","VII","VIII")
+        self.EnterSemester.config(bg="Black", fg="White")
         self.EnterSemester.place(x=790, y=360)
 
         self.ResetButton = Button(self.main_frame, text="Reset", activebackground="grey", bd=3, bg="White",
-                                         fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=8,command=self.Reset)
+                                         fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=8,command=self.ProceedPage)
         self.ResetButton.place(x=780, y=420)
 
         self.ContinueButton= Button(self.main_frame, text="Continue", activebackground="grey", bd=3, bg="White",
-                                         fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=8,command=self.Continue)
+                                         fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=8,command=self.ContinuePage)
         self.ContinueButton.place(x=650, y=420)
 
-        self.BackButton_Process = Button(self.main_frame, text="Back", activebackground="grey", bd=3, bg="White",
-                                 fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=8)
+        self.BackButton_Process = Button(self.main_frame, text="Back", activebackground="grey", bd=3, bg="White",fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=8)
         self.BackButton_Process.place(x=1100, y=680)
 
-    def Reset(self):
-        self.EnterSemester.delete("0","end")
-        self.EnterName.delete("0", "end")
-        self.EnterEnrollmentNo.delete("0", "end")
+##############################################################################################################
+####################################### 4th Page ######################################################################
+###############################################################################################################
 
-    def Continue(self):
+    def ContinuePage(self):
         self.Name = self.EnterName.get()
         self.RollNo = self.EnterEnrollmentNo.get()
+        self.Course = self.clicked.get()
+        self.Semester = self.SelectSemester.get()
         self.Name = self.Name.replace(" ", "")
-        if self.RollNo.isdigit() and int(len(self.RollNo)) == 11 and self.Name.isalpha():
-            self.main_frame.destroy()
-            self.create_MainFrame()
-            self.Check_Your_AttendanceButton = Button(self.main_frame, text="Check your attendance", activebackground="grey", bd=3, bg="White",
-                                         fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=25,command=self.Check_Attendance)
-            self.Check_Your_AttendanceButton.place(x=650, y=210)
+        if self.RollNo.isdigit() and int(len(self.RollNo)) == 11 and self.Name.isalpha() and self.Semester != "Select" and self.Course != "Select":
+            if self.Course == "BCA" and self.Semester == "VII":
+                self.Alert_1 = Label(self.main_frame,text="!There are only I TO VI \n Semester in BCA ",
+                                   bg="black", fg="white", font=self.TextFont)
+                self.Alert_1.place(x=860, y=360)
+            elif self.Course == "BCA" and self.Semester == "VIII":
+                self.Alert_1 = Label(self.main_frame,text="!There are only I TO VI \n Semester in BCA ",
+                                   bg="black", fg="white", font=self.TextFont)
+                self.Alert_1.place(x=860, y=360)
+            else:
+                self.main_frame.destroy()
+                self.create_MainFrame()
+                self.Check_Your_AttendanceButton = Button(self.main_frame, text="Check your attendance", activebackground="grey", bd=3, bg="White",
+                                         fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=25,command=self.Check_AttendancePage)
+                self.Check_Your_AttendanceButton.place(x=650, y=210)
 
-            self.Mark_Your_AttendanceButton= Button(self.main_frame, text="Mark your attendance", activebackground="grey", bd=3, bg="White",
-                                         fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=25,command=self.Mark_Attendance)
-            self.Mark_Your_AttendanceButton.place(x=650, y=310)
 
-            self.BackButton_Check= Button(self.main_frame, text="Back", activebackground="grey", bd=3, bg="White",
+                self.Mark_Your_AttendanceButton= Button(self.main_frame, text="Mark your attendance", activebackground="grey", bd=3, bg="White",
+                                         fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=25,command=self.Mark_AttendancePage)
+                self.Mark_Your_AttendanceButton.place(x=650, y=310)
+
+
+                self.BackButton_Check= Button(self.main_frame, text="Back", activebackground="grey", bd=3, bg="White",
                                              fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=8,command=self.ProceedPage)
-            self.BackButton_Check.place(x=1100, y=680)
+                self.BackButton_Check.place(x=1100, y=680)
         else:
-            self.EnterName.delete("0", "end")
-            self.EnterEnrollmentNo.delete("0", "end")
-            self.EnterSemester.delete("0", "end")
+            self.ProceedPage()
             self.Alert = Label(self.main_frame, text="!Your name should be name \n\n Enrollment number should be 11 digits only:", bg="black", fg="white", font=self.TextFont)
             self.Alert.place(x=590, y=70)
 
-    def Check_Attendance(self):
+##############################################################################################################
+############################################# 5th Page #######################################################
+##############################################################################################################
+
+    def Check_AttendancePage(self):
         self.main_frame.destroy()
         self.create_MainFrame()
 
         self.SelectSubject = Label(self.main_frame, text="Select subject :", bg="black", fg="white", font=self.TextFont)
         self.SelectSubject.place(x=600, y=200)
 
+        self.course = self.clicked.get()
+        self.semester = self.SelectSemester.get()
         self.ClickedSubject = StringVar(self.main_frame)
         self.ClickedSubject.set("Select")
-        self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "E.com", "CG","PHP","OS","ST","BE")
+        if self.course == "BCA":
+            if self.semester == "I":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "Maths-I", "Techical Communication","C language","ICIT","Physic")
+            elif self.semester == "II":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "Maths-II", "POM","DE","DS","DBMS")
+            elif self.semester == "III":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "Maths-III", "CA", "FEDT", "POA", "C++")
+            elif self.semester == "IV":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "Maths-IV", "Web.Tech","Java","SE","CN")
+            elif self.semester == "V":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "E.com", "CG","PHP","OS","ST","BE")
+            elif self.semester == "VI":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "DWH & DM","MC","Linux","M&IA","BI","AI","NS","NP")
+        elif self.course == "B.Tech":
+            if self.semester == "I":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "Maths-I", "Techical Communication","C language","ICIT","Physic-I")
+            elif self.semester == "II":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "Maths-II", "POM","DE","DS","DBMS")
+            elif self.semester == "III":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "Maths-III", "CA", "FEDT", "POA", "C++")
+            elif self.semester == "IV":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "Maths-IV", "Web.Tech","Java","SE","CN")
+            elif self.semester == "V":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "E.com", "CG","PHP","OS","ST","BE")
+            elif self.semester == "VI":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "DWH & DM","MC","Linux")
+            elif self.semester == "VII":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "DWH & DM","MC","Linux")
+            elif self.semester == "VIII":
+                self.SubjectEnter = OptionMenu(self.main_frame, self.ClickedSubject, "DWH & DM","MC","Linux")
         self.SubjectEnter.config(bg="Black", fg="White")
         self.SubjectEnter.place(x=820, y=200)
 
@@ -126,32 +199,26 @@ class AttendanceManager(object):
                                        fg="Black", font=self.ButtonFont, justify=RIGHT, height=1, width=8)
         self.BackButton_Check.place(x=1100, y=680)
 
+##############################################################################################################
+########################################## 6th Page ############################################################
+##############################################################################################################
+
     def ShowAttendance(self):
         self.subject = self.ClickedSubject.get()
         if self.subject == "Select":
             self.alert = Label(self.main_frame, text="!Select subject",bg="black", fg="white", font=self.TextFont)
             self.alert.place(x=600, y=100)
         else:
-            self.alert.destroy()
-            self.ShowSubjectAttendance = Label(self.main_frame, text="Your attenadance of " + self.subject + " is ", bg="black", fg="white", font=self.TextFont)
+           # self.alert.destroy()
+            self.ShowSubjectAttendance = Label(self.main_frame, text="Your attendance of " + self.subject + " is ", bg="black", fg="white", font=self.TextFont)
             self.ShowSubjectAttendance.place(x=600, y=350)
 
+############################################ 7th Page #########################################################
 
-    def Mark_Attendance(self):
+    def Mark_AttendancePage(self):
         self.main_frame.destroy()
         self.create_MainFrame()
 
-    def AboutPage(self):
-        self.content_frame.destroy() #Just destroy the content frame as the heading is needed
-        self.content_frame = Frame(self.main_frame, height=300, width=700, bg="Black")
-        self.content_frame.place(x=500, y=450)  # Place below heading
-        self.about = "Attendance Manager \n\n This is an application that can mark as well as manage your attendance. It confirms your identity by opening your webcam and using our face recognition model to confirm your identity and automatically mark your attendance.Also you can check your previous attendance. \n This application is developed by Arjun Bajaj and Anirudh Singh"
-        self.AboutInfo = Message(self.content_frame,bg="Black",fg="White",font=self.TextFont,justify=CENTER,width=600,
-                                 text=self.about)
-        self.AboutInfo.place(x=0,y=0)
-        self.BackButton = Button(self.content_frame, text="Back", activebackground="grey", bd=3, bg="White",fg="Black",
-                                    command=self.FirstPage, font=self.ButtonFont, justify=CENTER, height=1, width=5)
-        self.BackButton.place(x=400,y=250)
 
     def create_MainFrame(self,page="Any"):
         self.main_frame = Frame(self.root, height=800, width=1200)
@@ -169,8 +236,8 @@ class AttendanceManager(object):
 
 if __name__ == '__main__':
     AttendanceManager()
-#Before executing code, Python interpreter reads source file and define few special variables/global variables.
-#If the python interpreter is running that module (the source file) as the main program, it sets the special
+# Before executing code, Python interpreter reads source file and define few special variables/global variables.
+# If the python interpreter is running that module (the source file) as the main program, it sets the special
 # __name__ variable to have a value “__main__”. If this file is being imported from another module,
 # __name__ will be set to the module’s name. Module’s name is available as value to __name__ global variable.
-#Here our class DigitRecognizer script is being used as a module so its __name__ is __main__
+# Here our class DigitRecognizer script is being used as a module so its __name__ is __main__
