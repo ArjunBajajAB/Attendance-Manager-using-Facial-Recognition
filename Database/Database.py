0a7e7897b1d50979e3daa438cf23ab9e4a9e7d25
Arjun Bajaj <arjunbajaj2000@gmail.com>
2020-11-10 19:26:41 +0530

    Database files

---
+++ Database/Database.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import csv
+
+DataBase = pd.read_csv("DataBase.csv")
+
+SubjectsID = {
+    1:["CG","Ecom","OS","PHP"],
+    2:["CG","Ecom","OS","ST"],
+    3:["CG","Ecom","OS","BE"]
+}
