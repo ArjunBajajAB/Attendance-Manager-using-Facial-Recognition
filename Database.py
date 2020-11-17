b512ebd448ddadaea6608b0711e849a95f0dd811
Arjun Bajaj <arjunbajaj2000@gmail.com>
2020-11-17 20:53:09 +0530

    Testing

--- Database.py
+++ Database.py
@@ -5,21 +5,21 @@ import HelperFunctions
 
 DataBase = pd.read_csv("Database/DataBase.csv")
 
+SubjectsID = {
+    1: ["Maths_1", "TC", "ICIT", "Physics", "C"],
+    2: ["Maths_2", "POM", "DE", "DS", "DBMS"],
+    3: ["Maths_3", "CA", "FEDT", "POA", "C++"],
+    4: ["Maths_4", "WT", "Java", "SE", "CN"],
+    51: ["CG", "Ecom", "OS", "BE"],
+    52: ["CG", "Ecom", "OS", "ST"],
+    53: ["CG", "Ecom", "OS", "PHP"],
+    61: ["DWH & DM", "MC", "Linux", "BI"],
+    62: ["DWH & DM", "MC", "Linux", "NS"],
+    63: ["DWH & DM", "MC", "Linux", "NP"],
+    64: ["DWH & DM", "MC", "Linux", "AI"],
+    65: ["DWH & DM", "MC", "Linux", "Multimedia"]
+}
 def SubjectIdDict():
-    SubjectsID = {
-        1: ["Maths_1", "TC", "ICIT", "Physics", "C"],
-        2: ["Maths_2", "POM", "DE", "DS", "DBMS"],
-        3: ["Maths_3", "CA", "FEDT", "POA", "C++"],
-        4: ["Maths_4", "WT", "Java", "SE", "CN"],
-        51: ["CG", "Ecom", "OS", "BE"],
-        52: ["CG", "Ecom", "OS", "ST"],
-        53: ["CG", "Ecom", "OS", "PHP"],
-        61: ["DWH & DM", "MC", "Linux", "BI"],
-        62: ["DWH & DM", "MC", "Linux", "NS"],
-        63: ["DWH & DM", "MC", "Linux", "NP"],
-        64: ["DWH & DM", "MC", "Linux", "AI"],
-        65: ["DWH & DM", "MC", "Linux", "Multimedia"]
-    }
     return SubjectsID
 
 
